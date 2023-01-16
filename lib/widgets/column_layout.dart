b47637f44c0947748348d98feb8c697a770910fc
harshjainsk <harshjainsk531@gmail.com>
2023-01-17 01:49:36 +0530

    added custom widget for text in two columns

---
+++ lib/widgets/column_layout.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:gap/gap.dart';
+
+import '../utils/app_layout.dart';
+import '../utils/app_styles.dart';
+
+class ColumnLayout extends StatelessWidget {
+  final CrossAxisAlignment alignment;
+  final String firstText;
+  final String secondText;
+  const ColumnLayout({Key? key, required this.firstText, required this.secondText, required this.alignment}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: alignment,
+      children: [
+        Text(firstText, style: Styles.headLineStyle3.copyWith(fontWeight: FontWeight.normal, color: Colors.black)),
+        Gap(AppLayout.getHeight(5)),
+        Text(secondText, style: Styles.headLineStyle4.copyWith(color: Colors.grey),),
+      ],
+    );
+  }
+}
